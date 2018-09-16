7db125f7e476451fd04e59a02dc9faffb7aa1725
Robert Martin <robertmartinbc@shaw.ca>
2018-09-16 15:49:05 -0700

    Initialized rudimentary sign authentication.

---
+++ src/environment/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+   production: false,
+   firebase : {
+     apiKey: "AIzaSyDNPfUoogTHPzxtdKo_-kk4F835ZzTzj3U",
+     authDomain: "radiowrxbetatest.firebaseapp.com",
+     databaseURL: "https://radiowrxbetatest.firebaseio.com",
+     projectId: "radiowrxbetatest",
+     storageBucket: "radiowrxbetatest.appspot.com",
+     messagingSenderId: "1070813702872"
+   }
+ }
